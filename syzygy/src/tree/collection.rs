9729e8eae2b2090b0e0ba3c836a0dc9b60d21d72
Noah Kim <noahbkim@gmail.com>
2021-07-10 23:02:45 -0400

    Damn...

--- syzygy/src/tree/collection.rs
+++ syzygy/src/tree/collection.rs
@@ -5,6 +5,7 @@ use crate::router::{Path, Route};
 use crate::{Request, Response};
 use async_trait::async_trait;
 use std::sync::Arc;
+use crate::tree::view::{ViewRouter, ViewParent};
 
 #[async_trait]
 pub trait ItemView<S>: Send + Sync + 'static
@@ -16,12 +17,6 @@ where
     fn prepare(self: Arc<Self>, id: &str, state: Box<S>) -> Box<Route> {
         Box::new(move |request: Request| self.handle(request, id, state))
     }
-}
-
-pub trait CollectionView<S>: View<S> + ItemView<S>
-where
-    S: ?Sized + Send + Sync + 'static,
-{
 }
 
 pub trait CollectionViewTransition<S>
@@ -39,23 +34,23 @@ where
     fn child(&self, part: &str) -> Option<Box<dyn TreeNode<Self::T>>>;
 }
 
-pub trait CollectionViewRouter<S>
+pub trait ItemViewRouter<S>
 where
     S: ?Sized + Send + Sync + 'static,
 {
-    fn view(&self) -> Arc<dyn CollectionView<S>>;
+    fn view(&self) -> Arc<dyn ItemView<S>>;
 }
 
 impl<S, V> TreeNode<S> for V
 where
     S: ?Sized + Send + Sync + 'static,
-    V: CollectionViewRouter<S> + CollectionViewParent<S> + Send + Sync + 'static,
+    V: ItemViewRouter<S> + ViewRouter<S> + CollectionViewParent<S> + Send + Sync + 'static,
 {
     default fn route<'p>(&self, path: Path<'p>, state: Box<S>) -> Option<Box<Route>> {
         match Parts::from(path) {
-            Parts::Nil => Some(CollectionView::prepare(self.view(), state)),
+            Parts::Nil => Some(ViewRouter::view(&self).prepare(state)),
             Parts::Cons(id, rest) => match Parts::from(rest) {
-                Parts::Nil => Some(ItemView::prepare(self.view(), id, state)),
+                Parts::Nil => Some(ItemViewRouter::view(&self).prepare(id, state)),
                 Parts::Cons(part, rest) => match self.child(part) {
                     Some(child) => child.route(rest, V::T::from(state, id, part)),
                     None => None,
@@ -68,13 +63,13 @@ where
 impl<S, V> TreeNode<S> for V
 where
     S: ?Sized + Send + Sync + 'static,
-    V: CollectionViewRouter<S> + Send + Sync + 'static,
+    V: ItemViewRouter<S> + ViewRouter<S> + Send + Sync + 'static,
 {
     default fn route(&self, path: Path, state: Box<S>) -> Option<Box<Route>> {
         match Parts::from(path) {
-            Parts::Nil => Some(CollectionView::prepare(self.view(), state)),
+            Parts::Nil => Some(ViewRouter::view(&self).prepare(state)),
             Parts::Cons(id, rest) => match Parts::from(rest) {
-                Parts::Nil => Some(ItemView::prepare(self.view(), id, state)),
+                Parts::Nil => Some(ItemViewRouter::view(&self).prepare(id, state)),
                 _ => None,
             },
         }
